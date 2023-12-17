aa2d61fd916feebdb323d4ff1ee685a757106a3c
nicolasmeller <56871907+nicolasmeller@users.noreply.github.com>
2023-12-17 21:00:47 +0100

    .

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask, request 
 from Controllers import TaxContoller
 from dotenv import load_dotenv
+f
 load_dotenv()
 import os
 
 SECRET_KEY = os.getenv("MY_SECRET")
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['DATABASE_URL'] = os.environ.get('DATABASE_URL')
-
 
 
 @app.route('/')
